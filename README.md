db0f94737a7e34b8a6fa516f58e59e72b17a5d17
samaryasser <73972856+samaryasser@users.noreply.github.com>
2022-12-19 23:17:15 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Landing Page Project
+Landing page is the conversion of a ststic page that allows some interaction.
+The programing language used:
+HTML.
+CSS.
+JavaScript.
+
+
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project.I convert this project from a static project to an interactive one. 
+Starter code taken from Udacity( https://github.com/udacity/fend.git )
+
+This project has the scope of converting  a static web page into an interactive page.
+
---
+++ app.js
@@ -0,0 +1,84 @@
+/**
+ * creating sections dynamically
+ * by using createSection function and add them to the main tag
+ * ES6
+ */
+// counter to specify attributes and number of section
+let counter = 0;
+const createSection = () => {
+  counter++;
+  const content = `<section id="section${counter}" data-nav="Section ${counter}">
+    <div class="landing__container">
+    <h2>Section ${counter}</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+    
+    <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+    </div>
+    </section>`;
+  document.querySelector("main").insertAdjacentHTML("beforeend", content);
+};
+/**
+ * make list items equal to the number of sections by iterate on them
+ * but I need to remove all items to avoid the duplicating
+ */
+const navBar = document.getElementById("navbar__list");
+const createNavItems = () => {
+  navBar.innerHTML = "";
+  document.querySelectorAll("section").forEach((section) => {
+    const listItem = `<li><a href="#${section.id}" data-nav="${section.id}" class="menu__link">${section.dataset.nav}</a></li>`;
+    navBar.insertAdjacentHTML("beforeend", listItem);
+  });
+};
+
+window.onscroll = function() {
+	document.querySelectorAll("section").forEach(function(active) {
+    let activeLink = navBar.querySelector(`[data-nav=${active.id}]`);
+	if(active.getBoundingClientRect().top >= -400 && active.getBoundingClientRect().top <= 150){
+
+    active.classList.add("your-active-class");
+    activeLink.classList.add("active-link");
+
+    }
+    else{
+         active.classList.remove("your-active-class");
+         activeLink.classList.remove("active-link");
+    }
+	});
+}
+
+/**
+ * when you click on nav links will go smoothly to the correct section
+ * i can shortcut this code just using CSS (html{ scroll-behavior: "smooth"})
+ * but I think it better to use what I learn
+ */
+navBar.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.dataset.nav) {
+    document
+      .getElementById(`${event.target.dataset.nav}`)
+      .scrollIntoView({ behavior: "smooth" });
+    setTimeout(() => {
+      location.hash = `${event.target.dataset.nav}`;
+    }, 200);
+  }
+});
+
+/**
+ * create four-section dynamically by javascript instead of HTML
+ * create them links
+ * ability to observe sections
+ */
+for (let i = 1; i < 5; i++) createSection();
+createNavItems();
+
+// creating more sections by click on the button
+document.getElementById("btn").addEventListener("click", () => {
+  createSection();
+  createNavItems();
+  
+});
+
+
+
+
+
